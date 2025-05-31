0323011305b221502b64a170e64c7543419c27ee
Tharushi <163126235+tharushi10@users.noreply.github.com>
2025-05-31 19:44:23 +0530

    main function

--- Program1.c
+++ Program1.c
@@ -13,36 +13,4 @@ char Circular_Queue_rotate(char charecter, int shift_value, int encrypt_message)
         return (char)(base + (charecter - base - shift_value + SIZE) % SIZE); //process Decrypt message.
 }
 
-int main() {
-    char message[200];
-    int shift_value, i;
-    char choice;
 
-    printf("Choose (E)ncrypt or (D)ecrypt: ");
-    scanf(" %c", &choice);
-
-    printf("Enter shift key (Any interger): ");
-    scanf("%d", &shift_value);
-
-    getchar(); // clear leftover newline
-
-    printf("Enter your message: ");
-    fgets(message, sizeof(message), stdin);
-    message[strcspn(message, "\n")] = '\0'; // remove newline
-
-    printf("********************\nResult: ");
-    for (i = 0; message[i] != '\0'; i++) {
-        if (isalpha(message[i])) {
-            if (choice == 'E' || choice == 'e')
-                printf("%c", Circular_Queue_rotate(message[i], shift_value, 1));
-            else if (choice == 'D' || choice == 'd')
-                printf("%c", Circular_Queue_rotate(message[i], shift_value , 0));
-        } else {
-            printf("%c", message[i]); // print symbols spaces as-is
-        }
-    }
-
-    printf("\n");
-
-    return 0;
-}
